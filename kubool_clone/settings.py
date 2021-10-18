22533256cffb50427396a9aee293d6a54c918c72
TropicolCoder <jacobsbusayo@gmail.com>
2021-10-18 13:57:49 +0100

    static fix

--- kubool_clone/settings.py
+++ kubool_clone/settings.py
@@ -159,5 +159,5 @@ DATABASES['default'].update(prod_db)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
