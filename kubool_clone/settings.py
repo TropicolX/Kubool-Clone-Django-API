c39fa11e2823582cf741ed0bb62b968f44e5e4ae
TropicolCoder <jacobsbusayo@gmail.com>
2021-10-18 10:28:26 +0100

    staticfiles fix

--- kubool_clone/settings.py
+++ kubool_clone/settings.py
@@ -155,4 +155,5 @@ CORS_ALLOW_ALL_ORIGINS=True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
