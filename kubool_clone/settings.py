467c3e4beb5c93b629d0981df876b533ea4516b6
TropicolCoder <jacobsbusayo@gmail.com>
2021-10-19 20:12:00 +0100

    blacklist after rotation

--- kubool_clone/settings.py
+++ kubool_clone/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "BLACKLIST_AFTER_ROTATION": True
 }
 
 # swagger docs settings
